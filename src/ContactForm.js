6db44e1c1714553ff796374d7235b0a7ee10b045
acteiongithub <admin@acteion.com>
2024-08-29 18:52:33 +0530

    Update ContactForm.js

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -62,7 +62,7 @@ const ContactForm = () => {
         e.preventDefault();
         if (validate()) {
             try {
-                const res = await axios.post('http://localhost:5000/api/contacts', formData);
+                const res = await axios.post('http://44.202.167.124:3000/api/contacts', formData);
                 alert(res.data);
                 setFormData({
                     firstname: '',
